859a2165c4fb3949e7077a78eb255c1fcc1911ed
Gerardo Duran-Martin <g.duran@me.com>
2023-12-24 15:43:19 +0000

    refactor: jfin.py

--- 06-reinforcement-learning/jfin.py
+++ 06-reinforcement-learning/jfin.py
@@ -11,11 +11,11 @@ class BanditState:
     value: jax.Array
     key: jax.random.PRNGKey
     
-    def update(self, action, value, count):
+    def update(self, ix, value, count):
         new_key = jax.random.split(self.key)[1]
         state = self.replace(
-            value=self.value.at[action].set(value),
-            count=self.count.at[action].set(count),
+            value=self.value.at[ix].set(value),
+            count=self.count.at[ix].set(count),
             key=new_key,
         )
         return state
@@ -29,7 +29,7 @@ class EpsilonGreedyAgent:
     def _step(self, state, rewards, storefn):
         key_choice, key_arm = jax.random.split(state.key)
         is_greedy = jax.random.bernoulli(key_choice, p=1 - self.epsilon) # w.r.t. the value function
-        n_bandits = state.value.shape[0]
+        n_bandits = state.value.shape[-1]
         random_choice = jax.random.choice(key_arm, n_bandits)
         
         action = state.value.argmax() * is_greedy + random_choice * (1 - is_greedy) 
@@ -40,7 +40,9 @@ class EpsilonGreedyAgent:
         new_value = state.value[action] + (reward - state.value[action]) * discount
         
         state = state.update(action, new_value, new_count)
-        return state, storefn(state, action, reward)
+        carry = storefn(state, action, reward)
+
+        return state, carry
     
     def init(self, key, n_arms):
         count_v = jnp.zeros(n_arms)
@@ -48,20 +50,19 @@ class EpsilonGreedyAgent:
         state = BanditState(count=count_v, value=value_v, key=key)
         return state
 
-
     @partial(jax.jit, static_argnames=("self", "storefn",))
-    def init_and_run(self, key, data, storefn):
-        state = self.init(key, data.shape[1])
+    def init_and_run(self, key, rewards, storefn):
+        state = self.init(key, rewards.shape[1])
         partial_step = partial(self._step,  storefn=storefn)
-        state, hist = jax.lax.scan(partial_step, state, data)
+        state, hist = jax.lax.scan(partial_step, state, rewards)
         return state, hist
     
 
     @partial(jax.jit, static_argnames=("self", "storefn", "n_sims"))
-    def init_and_run_sims(self, key, data, storefn, n_sims):
+    def init_and_run_sims(self, key, rewards, storefn, n_sims):
         keys = jax.random.split(key, n_sims)
         vmap_returns = jax.vmap(self.init_and_run, in_axes=(0, None, None))
-        state, hist = vmap_returns(keys, data, storefn)
+        state, hist = vmap_returns(keys, rewards, storefn)
         return state, hist
     
 
